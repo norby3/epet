d2efa1f9e0b87681899bdfed4d86cb9210c4d22c
Norby Ryan <norby@epetfolio.com>
2012-08-11 19:51:41 -0400

    caretakers also had string instead of int

--- db/migrate/20120726163416_create_caretakers.rb
+++ db/migrate/20120726163416_create_caretakers.rb
@@ -1,9 +1,9 @@
 class CreateCaretakers < ActiveRecord::Migration
   def change
     create_table :caretakers do |t|
-      t.string :pet_id
-      t.string :person_id
-      t.string :organization_id
+      t.integer :pet_id
+      t.integer :person_id
+      t.integer :organization_id
       t.string :primary_role
       t.string :secondary_role
       t.string :started_at
