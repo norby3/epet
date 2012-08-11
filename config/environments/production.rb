e267a0cf9d403d6d03692777af8a486b48edc310
Norby Ryan <norby@epetfolio.com>
2012-08-11 16:45:09 -0400

    mail config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,14 @@ Epet5::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.default_url_options = { :host => 'calm-falls-3515.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'calm-falls-3515.herokuapp.com' }
   # config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.default_url_options = { :host => "localhost:3000" }  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+
 
   # Enable threaded mode
   # config.threadsafe!
