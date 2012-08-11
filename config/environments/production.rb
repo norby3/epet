cbc916688da9f267ca1dd9e7ef1ec61dd4f413b6
Norby Ryan <norby@epetfolio.com>
2012-08-11 16:24:20 -0400

    mail config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Epet5::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
+  config.action_mailer.default_url_options = { :host => 'calm-falls-3515.herokuapp.com/' }
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
@@ -64,4 +65,5 @@ Epet5::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
 end
--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,13 +1,13 @@
 
-# ActionMailer::Base.smtp_settings = {
-#   :address              => 'smtp.gmail.com',
-#   :port                 => 587,
-#   :domain               => 'epetfolio.com',
-#   :user_name            => 'epetfolio@gmail.com',
-#   :password             => 'fluffyfid0',
-#   :authentication       => :plain,
-#   :enable_starttls_auto => true
-# }
+ActionMailer::Base.smtp_settings = {
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  :domain               => 'epetfolio.com',
+  :user_name            => 'epetfolio@gmail.com',
+  :password             => 'fluffyfid0',
+  :authentication       => :plain,
+  :enable_starttls_auto => true
+}
 
 
 #ActionMailer::Base.default_url_options[:host] = "www.epetfolio.com"
