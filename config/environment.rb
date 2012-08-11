c57dbe0a891ee801bd0c634ba4197a9a7d236c74
Norby Ryan <norby@epetfolio.com>
2012-08-11 16:04:03 -0400

    smtp mail config in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Epet5::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  #:domain               => 'epetfolio.com',
+  :user_name            => 'epetfolio@gmail.com',
+  :password             => 'fluffyfid0',
+  :authentication       => :plain,
+  :enable_starttls_auto => true
+}
