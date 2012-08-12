c19b3bb4922b6e14db901a2a4fbcaa2e588bacbd
Norby Ryan <norby@epetfolio.com>
2012-08-12 19:18:59 -0400

    config vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => :access_key_id,       # required
-    :aws_secret_access_key  => :secret_access_key,       # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
     :region                 => 'US Standard'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'epetfolio'                     # required
