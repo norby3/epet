9b77f50e75e10385c9eab0c24745c1593d1106d2
Norby <norbert.ryan3@gmail.com>
2012-10-21 13:52:40 -0400

    add email to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Epet5::Application.routes.draw do
 
   # trying to setup redirect to open the mobile app
   # the operating system should see PetOwner:// and then want to open PetOwneriOS
-  match "friendsfamily/:token" => redirect("PetOwner://epetfolio/%{token}")
+  match "friendsfamily/:email/:token" => redirect("PetOwner://epetfolio/%{email}/%{token}")
 
   # these did not work...
   #  match "friendsfamily/:verify_email_token" => redirect {|params| "PetOwner://#{params[:verify_email_token]}" }
